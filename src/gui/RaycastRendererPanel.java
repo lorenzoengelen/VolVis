e9f1dbe5ef12c84c7e1763291a9c0874e5e57314
Lorenzo Engelen <lorenzoengelen@gmail.com>
2016-12-10 21:41:01 +0100

    (refactor) changed name 2DTransferFunction

--- src/gui/RaycastRendererPanel.java
+++ src/gui/RaycastRendererPanel.java
@@ -148,7 +148,7 @@ public class RaycastRendererPanel extends javax.swing.JPanel {
     }//GEN-LAST:event_compositingButtonActionPerformed
 
     private void tf2dButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_tf2dButtonActionPerformed
-        renderer.setRenderer("2DTransfer");
+        renderer.setRenderer("TransferFunction2D");
         renderer.changed();
     }//GEN-LAST:event_tf2dButtonActionPerformed
 
