bc07438277c1e4d5b6691f0d21860ee29e376f3d
Lorenzo Engelen <lorenzoengelen@gmail.com>
2016-12-10 15:56:31 +0100

    (feat) paste code of slicer in mip

--- src/volvis/RaycastRenderer.java
+++ src/volvis/RaycastRenderer.java
@@ -188,7 +188,35 @@ public class RaycastRenderer extends Renderer implements TFChangeListener {
     
     // == MIP ==============================================
     void mip(double[] viewMatrix) {
+        
         generalSetup(viewMatrix);
+        
+        for (int j = 0; j < image.getHeight(); j++) {
+            for (int i = 0; i < image.getWidth(); i++) {
+                pixelCoord[0] = uVec[0] * (i - imageCenter) + vVec[0] * (j - imageCenter) + volumeCenter[0];
+                pixelCoord[1] = uVec[1] * (i - imageCenter) + vVec[1] * (j - imageCenter) + volumeCenter[1];
+                pixelCoord[2] = uVec[2] * (i - imageCenter) + vVec[2] * (j - imageCenter) + volumeCenter[2];
+
+                double val = getVoxel(pixelCoord);
+
+                // Map the intensity to a grey value by linear scaling
+                voxelColor.r = val / max;
+                voxelColor.g = voxelColor.r;
+                voxelColor.b = voxelColor.r;
+                voxelColor.a = val > 0 ? 1.0 : 0.0;  // this makes intensity 0 completely transparent and the rest opaque
+                // Alternatively, apply the transfer function to obtain a color
+                // voxelColor = tFunc.getColor(val);
+
+                // BufferedImage expects a pixel color packed as ARGB in an int
+                int c_alpha = voxelColor.a <= 1.0 ? (int) Math.floor(voxelColor.a * 255) : 255;
+                int c_red = voxelColor.r <= 1.0 ? (int) Math.floor(voxelColor.r * 255) : 255;
+                int c_green = voxelColor.g <= 1.0 ? (int) Math.floor(voxelColor.g * 255) : 255;
+                int c_blue = voxelColor.b <= 1.0 ? (int) Math.floor(voxelColor.b * 255) : 255;
+                int pixelColor = (c_alpha << 24) | (c_red << 16) | (c_green << 8) | c_blue;
+                image.setRGB(i, j, pixelColor);
+            }
+        }
+        
     }
     
     // clear image
