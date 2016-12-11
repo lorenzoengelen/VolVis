c813a957282ac91dd79cdb4e71b96c59ddca35c9
Lorenzo Engelen <lorenzoengelen@gmail.com>
2016-12-11 12:05:14 +0100

    (bug) fixed out of bounds bug in GradientVolume

--- src/volume/GradientVolume.java
+++ src/volume/GradientVolume.java
@@ -21,10 +21,6 @@ public class GradientVolume {
     }
 
     public VoxelGradient getGradient(int x, int y, int z) {
-        
-//        if (x < 0 || x >= dimX || y < 0 || y >= dimY || z < 0 || z >= dimZ) {
-//            return new VoxelGradient(0, 0, 0);
-//        }
         return data[x + dimX * (y + dimY * z)];
     }
 
@@ -56,24 +52,33 @@ public class GradientVolume {
     private void compute() {
 
 //         this just initializes all gradients to the vector (0,0,0)
-        for (int i=0; i<data.length; i++) {
-            data[i] = zero;
-        }
-                
-//        for (int i = 0; i < dimX; i++) {
-//            for (int j = 0; j < dimY; j++) {
-//                for (int k = 0; k < dimZ; k++) {
-//                    
-//                    float x = (float) (0.5 * (volume.getVoxel(i - 1, j, k) - volume.getVoxel(i + 1, j, k)));
-//                    float y = (float) (0.5 * (volume.getVoxel(i, j - 1, k) - volume.getVoxel(i, j + 1, k)));
-//                    float z = (float) (0.5 * (volume.getVoxel(i, j, k - 1) - volume.getVoxel(i, j, k + 1)));
-//                    
-//                    VoxelGradient gradient = new VoxelGradient(x, y, z);
-//                    setGradient(i, j, k, gradient);
-//                    
-//                }
-//            }
+//        for (int i=0; i<data.length; i++) {
+//            data[i] = zero;
 //        }
+                
+        for (int i = 0; i < dimX; i++) {
+            for (int j = 0; j < dimY; j++) {
+                for (int k = 0; k < dimZ; k++) {
+                    
+                    VoxelGradient gradient = new VoxelGradient(0, 0, 0);
+                    
+                    if (i == 0 || i == dimX - 1 || j == 0 || j == dimY - 1 || k == 0 || k == dimZ -1) {
+                        
+                        setGradient(i, j, k, gradient);
+                        
+                    } else {
+                     
+                        float x = (float) (0.5 * (volume.getVoxel(i - 1, j, k) - volume.getVoxel(i + 1, j, k)));
+                        float y = (float) (0.5 * (volume.getVoxel(i, j - 1, k) - volume.getVoxel(i, j + 1, k)));
+                        float z = (float) (0.5 * (volume.getVoxel(i, j, k - 1) - volume.getVoxel(i, j, k + 1)));
+                        
+                        gradient = new VoxelGradient(x, y, z);
+                        setGradient(i, j, k, gradient);
+                    }
+                    
+                }
+            }
+        }
 
     }
     
